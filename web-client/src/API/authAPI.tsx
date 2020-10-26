245e1e80aa1d1cec9ab6860b17129addabb26f57
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-10-26 16:45:18 +0300

    issues1

--- web-client/src/API/authAPI.tsx
+++ web-client/src/API/authAPI.tsx
@@ -1,35 +1,37 @@
-
-
-export function login(username: string, password: string){
-    const requestOptions = {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({username, password})
-    }
-    return fetch('api/identity/login', requestOptions)
-    .then(user => {
-        localStorage.setItem('currentUser', JSON.stringify(user));
-
-        return user;
-    })
-
-}
-
-export function register(username: string, password: string) {
-    const requestOptions = {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({username, password})
-    }
-    return fetch('api/identity/register', requestOptions)
-    .then(user => {
-        localStorage.setItem('currentUser', JSON.stringify(user));
-
-        return user;
-    })
+export class AuthService{
+    login(login: string, password: string){
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({login, password})
+        }
+        return fetch('api/identity/login', requestOptions)
+        .then(user => {
+            localStorage.setItem('currentUser', JSON.stringify(user));
     
+            return user;
+        })
+    
+    }
+    
+    register(username: string, password: string) {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({username, password})
+        }
+        return fetch('api/identity/register', requestOptions)
+        .then(user => {
+            localStorage.setItem('currentUser', JSON.stringify(user));
+    
+            return user;
+        })
+        
+    }
+    
+    logout(){
+        localStorage.removeItem('currentUser')
+    }
 }
 
-export function logout(){
-    localStorage.removeItem('currentUser')
-}
+
--- web-client/src/components/login.tsx
+++ web-client/src/components/login.tsx
@@ -1,34 +1,51 @@
 import React from 'react';
 
 import '../styles/login.scss'
+import { AuthService } from '../API/authAPI'
 
-export default class Login extends React.Component{
-    state = {
-        username: "",
-        password: ""
+export default class Login extends React.Component<{}, { login: string, password: string }>{
+    constructor(props: any, private pageService: AuthService) {
+        super(props);
+        this.state = {
+            login: '',
+            password: ''
+        };
+
+        this.onChangeLogin = this.onChangeLogin.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this)
     }
 
-    onUsernameChange=(event: { target: { value: any; }; })=>{
-        this.setState({username: event.target.value})
+    onChangeLogin(event: any){
+        this.setState({login: event.target.value})
     }
 
-    onPasswordChange=(event: { target: { value: any; }; })=>{
+    onChangePassword(event: any){
         this.setState({password: event.target.value})
+    }
+
+    onSubmit(event: any){
+        alert("Welcome");
+        event.preventDefault();
+        console.log(this.state.login, this.state.password);
+        this.pageService.login(this.state.login, this.state.password);
     }
 
     render(){
         return(<>
         <div className="container">
             <div className="loginContainer">
-                <div className="imgContainer">
-
-                </div>
                 <div className="formContainer">
-                    <p>SignIn</p>
-                    <form action="" onSubmit = {this.handleSubmit}>
-                        <input type="text" placeholder="login" value={this.state.username} onInput={this.onUsernameChange}/>
-                        <input type="text" placeholder="password" value={this.state.password} />
-                        <input type="submit button" value="SignIn" />
+                    <form onSubmit={this.onSubmit}>
+                        <div className="formGroup">
+                            <p className="inputDescription">Логин</p>
+                            <input className="input" type="text" name="login" value={this.state.login}onChange={this.onChangeLogin}/>
+                        </div>
+                        <div className="formGroup">
+                            <p className="inputDescription">Пароль</p>
+                            <input className="input" type="password" name="password" value={this.state.password}onChange={this.onChangePassword}/>
+                        </div>
+                        <p><input className = "subButton" type="submit" value="Submit" /></p>
                     </form>
                 </div>
             </div>
