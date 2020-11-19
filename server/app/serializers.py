a31ca657d61f50cf0f4573140c6c65132fa04ea0
AndyS1mpson <whale942999@gmail.com>
2020-11-19 19:26:56 +0300

    fix create user controller

--- server/app/serializers.py
+++ server/app/serializers.py
@@ -9,8 +9,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = ['name','second_name','description','company','phone_number']
 
-
-
 class UserSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer(many=False)
     class Meta:
@@ -20,13 +18,19 @@ class UserSerializer(serializers.ModelSerializer):
     
     def create(self,validated_data):        
         profile_data = validated_data.pop('profile')
-        profile = ProfileSerializer.create(ProfileSerializer(), validated_data=profile_data)
         
-        user, created = User.objects.update_or_create(profile = profile,
-                                                        username=validated_data['username'],
-                                                        email=validated_data['email'])
+        user, created = User.objects.update_or_create(username=validated_data['username'],
+                                                      email=validated_data['email'])
         user.set_password(validated_data['password'])
         user.save()
+
+        profile = Profile.objects.create(user=user,
+                                         name=profile_data['name'],
+                                         second_name=profile_data['second_name'],
+                                         description=profile_data['description'],
+                                         company=profile_data['company'],
+                                         phone_number=profile_data['phone_number'])
+        
         return user
 
 class RetriewCreateDestroyUserSerializer(serializers.ModelSerializer):
@@ -53,8 +57,8 @@ class RetriewCreateDestroyUserSerializer(serializers.ModelSerializer):
         return instance
 
 
-class ProfileViewSerializer(serializers.ModelSerializer):
-    user = UserSerializer(many = False)
-    class Meta:
-        model = Profile
-        fields = ['user','name','second_name','description','company','phone_number','rating','is_admin','created_date']
+# class ProfileViewSerializer(serializers.ModelSerializer):
+#     user = UserSerializer(many = False)
+#     class Meta:
+#         model = Profile
+#         fields = ['user','name','second_name','description','company','phone_number','rating','is_admin','created_date']
